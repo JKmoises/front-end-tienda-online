40f4c128fbebfc1e00a8e0f38ba601d606458a3c
JKmoises <moi.prado@duocuc.cl>
2022-11-11 13:56:21 -0300

    Agregando componente de producto

--- app/components/ProductCard.js
+++ app/components/ProductCard.js
@@ -1,5 +1,18 @@
 
 
-export function ProductCard(){
+export function ProductCard(props) {
+  let { id, name, price,url_image, discount } = props;
   
+  return /*html*/`
+    <article class="product-card">
+      <img class="product-card__image" src="${url_image}" alt="${name}">
+      <h2 class="product-card__name">${name}</h2>
+      
+      <div class="shopping-container">
+        <span class="shopping-container__price">$${price}</span>
+        <button class="shopping-container__btn" data-id="${id}">
+          <i class="fas fa-cart-plus"></i>
+        </button>
+      </div>
+    </article>`;
 }
