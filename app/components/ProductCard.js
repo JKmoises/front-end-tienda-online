c061e3332bf86700b9ac7c7fdd3ed74a7f99ddb5
JKmoises <moi.prado@duocuc.cl>
2022-11-11 15:07:53 -0300

    Estilizando componente de producto y aplicando responsive design

--- app/components/ProductCard.js
+++ app/components/ProductCard.js
@@ -8,12 +8,12 @@ export function ProductCard(props) {
   return /*html*/`
     <article class="product-card">
       <img class="product-card__image" src="${url_image}" alt="${name}">
-      <h2 class="product-card__name">${name}</h2>
+      <h4 class="product-card__name">${name}</h4>
       
       <div class="shopping-container">
         <p>
           <span class="shopping-container__price">$${price}</span>
-          <sup>${priceDiscount}</sup>
+          <sup class="shopping-container__discount">${priceDiscount}</sup>
         </p>
         <button class="shopping-container__btn" data-id="${id}">
           <i class="fas fa-cart-plus"></i>
