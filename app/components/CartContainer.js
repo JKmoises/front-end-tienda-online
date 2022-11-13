d4716ce14927097b3802ea89ad38c6f63a6cc906
JKmoises <moi.prado@duocuc.cl>
2022-11-13 19:01:57 -0300

    Estilizando contenedor de carrito

---
+++ app/components/CartContainer.js
@@ -0,0 +1,33 @@
+
+export function CartContainer(){
+  const $cartContainer = document.createElement('section');
+  $cartContainer.id = 'cart-container';
+  $cartContainer.classList.add('cart-container');
+  
+  const $cleanBtn = document.createElement('button');
+  $cleanBtn.id = 'cart-container__clean-btn';
+  $cleanBtn.classList.add('cart-container__clean-btn');
+  $cleanBtn.textContent = 'Limpiar Carrito';
+
+  const $totalCart = document.createElement('div');
+  $totalCart.classList.add('cart-container__total');
+  $totalCart.innerHTML = /*html*/`
+    <h2>Total: <span class="cart-container__total--buy"></span></h2>
+  `;
+
+  const $buyBtn = document.createElement('button');
+  $buyBtn.id = 'cart-container__buy-btn';
+  $buyBtn.classList.add('cart-container__buy-btn');
+  $buyBtn.textContent = 'Terminar Compra';
+
+
+  $cartContainer.appendChild($cleanBtn);
+  $cartContainer.appendChild($totalCart);
+  $cartContainer.appendChild($buyBtn);
+
+
+
+  return $cartContainer;
+
+
+}
--- app/components/Navegation.js
+++ app/components/Navegation.js
@@ -1,12 +1,16 @@
+import { CartContainer } from "./CartContainer.js";
+
 export function Navigation(){
   const $nav = document.createElement('nav');
   $nav.classList.add('navigation');
 
   $nav.innerHTML = /*html*/`
     <a id="carrito-icon" class="navigation__cart" title="Haga click para fijar Carrito">
-      <i class="fas fa-cart-plus"></i>
+      <i class="navigation__icon fas fa-cart-plus"></i>
     </a>
   `;
+
+  $nav.appendChild(CartContainer());
 
   return $nav;
 }
